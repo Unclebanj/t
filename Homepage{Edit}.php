79a739ac57e43c7601e2f4637d493e0f06263550
Unclebanj <timothyodufuwa.to@gmail.com>
2017-04-29 00:34:12 +0100

    Add files via upload

---
+++ Homepage{Edit}.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>KU Talent CV Website</title>
+     <link href="maincssstyle.css" rel="stylesheet" href = "12-column-grid.css"/>
+	 
+ <script type = "text/javascript">
+	  
+	  function help(){
+	   window.location.href = 'help.php';
+	  }
+	  function about(){
+	  window.location.href = 'about.php';
+	  }
+	  function contact(){
+	    window.location.href = 'contact.php';
+	  } 
+	  function sendcv(){
+	  window.location.href = 'sauthenticate.php';
+	  }
+ </script>	
+  </head>
+  <body>
+  <div class = "container">
+  <div class ="row">
+   <div class = "header">
+    <img src = "logo.jpg" alt = "logo"/> 
+      <h4> KU TALENT</h4>
+  <button><a href = "authenticate.php">Log In</a></button>
+</div>
+  </div>
+  <div class ="row">
+    <nav>
+	 <ul>
+	 <li><input type = "button" title="This is a help button" value = "HELP" name = "button1" onclick = "help()"/></li>
+	 <li><input type = "button" title = " About KU Talent" value = "ABOUT" name = "button2" onclick = "about()"/></li>
+	  <li><input type = "button" title = "Contact KU Talent" value = "CONTACT" name = "button3" onclick = "contact()"/></li>
+	   <li><input type = "button" title = "CV Submission" value = "SEND A CV" name = "button4" onclick = "sendcv()"/></li>
+	 </ul>
+	 </nav> 
+  </div>
+  <div class ="row">
+   <div class="row3">
+    <div class = "nine">
+  <div class="section1">
+	 <h3>Job Search</a>
+	 <form><input type= "text" name ="search" placeholder ="Job Title"></form>
+	 <button style ="top:5px;position:relative;"><a href="" style="text-decoration:none;">SEARCH</a></button>
+	 <table style = "border-collapse:collapse;">
+	 <tr>
+	 <th>Keep In touch with Employers</th>
+	 </tr>
+	 <tr>
+	 <td><p style = "font-size:15px;">Gain experience in the industry and maintain a good network. Click for <a href ="">more</a> information</p></td>
+	 </tr>
+	 </table>
+      </div>
+  </div>
+  <div class = "three">
+  
+  </div>
+  </div>
+    </div>
+  <div class ="row4">
+  <div class ="one">
+    <div class = "nav1">
+     <nav>
+	   <ul>
+	   <li title = "Summer jobs"><a href ="https://jobscentral.kingston.ac.uk/">Summer Internships</a></li>
+	    <li title = "Grad Jobs"><a href ="https://jobscentral.kingston.ac.uk/">Graduate Schemes</a></li>
+		 <li title ="Placements"><a href ="https://jobscentral.kingston.ac.uk/">Work Placements</a></li>
+		  <li title = "Part-time work"><a href ="https://jobscentral.kingston.ac.uk/">Part Time Jobs</a></li>
+		   <li title = "Engage in voluntary work"><a href ="https://jobscentral.kingston.ac.uk/">Voluntary Jobs</a></li>
+	   </ul>
+	   </nav>
+  </div>
+  </div>
+  </div>
+  <div class ="row">
+  <div class = "section2">
+     <div class = "one">
+  <table style = "width:275px; height:200px;margin:10px;background:linear-gradient(silver,cadetblue,black);float:left;">
+	 <tr>
+	 <th>Browse By Faculty</th>
+	 </tr>
+	<tr>
+	<td><p style = "font-size:15px; font-family:book antiqua; color:white;"> </p></td>
+	</tr>
+	  <tr>
+	   <td><button style = "width:250px;">Health and Social Care</button></td>
+	   </tr>
+	     <tr>
+	 <td><button style = "width:250px;">Art and Social Science</button></td>
+	  </tr>
+	   <tr>
+	   <td><button style ="width:250px;">Art Design and Architecture</button></td>
+	  </tr>
+	 <tr>
+	 <td><button style = "width:250px;">Social Engineering and Computing</button></td>
+	 </tr>
+	 </table>
+	  <table style = "width:280px; height:200px;margin:10px;background:linear-gradient(silver,cadetblue, black);float:left;">
+	 <tr>
+	 <th>Create Job Alerts</th>
+	 </tr>
+	 <tr>
+	 <td><p style = "font-size:15px;color:white;font-family:book antiqua">Set up an alert for the work experience you are seeking for</p>
+	 <button style = "text-decoration:none; position:relative; left:10px;">Create Alerts</button></td>
+	 </tr>
+	 </table>
+	 <table style = "width:280px; height:200px;margin:10px;background:linear-gradient(silver, cadetblue, black);float:left;">
+	 <tr>
+	 <th>KU Talent Survey</th>
+	 </tr>
+	 <tr> 
+	 <td><p style = "font-size:15px;color:white;font-family:book antiqua">Tell KU Talent what you think by taking part in our survey</p>
+	 <button style = "text-decoration:none; position:relative; left:10px;">Click to start survey</button></td>
+	 </tr>
+	 </table>
+	 </div>
+  </div>
+  </div>
+  
+  <div class ="row5">
+    <div class = "three">
+	<div class = "sidebar1">
+	 <iframe src="https://www.youtube.com/embed/rqE2UHzNcQM" frameborder="0" allowfullscreen></iframe>
+	 <table>
+	 <tr>
+	 <th>SEE WHATS NEW AT KINGSTON UNIVERSITY</th>
+	 </tr>
+	 <tr>
+	 <td style = "padding:25px;">Job Opportunities at the University.</td>
+	 <tr>
+	 <td>
+	 <form>
+	 <input type="checkbox" name="">Making Nature Exhibition<br>
+	 <input type="checkbox" name="">Freelancing<br>
+	 <input type="checkbox" name="">Working Abroad<br>
+	 <input type="checkbox" name="">KU Commuter Work Experience<br>
+	 </form>
+	 </tr>
+	 </tr>
+	 </table>
+	 </div>
+  </div>
+  </div>
+  <div class ="row6">
+    <div class = "nine">
+	<div class = "sidebar2">
+	<table>
+	 <tr>
+	 <th>UPCOMING JOB EVENTS</th>
+	 </tr>
+	 <tr>
+	 <td>Job Opportunities at the University.</td>
+	 <tr>
+	 <td>
+	 <form>
+	 <input type="checkbox" name="">Making Nature Exhibition<br>
+	 <input type="checkbox" name="">Freelancing<br>
+	 <input type="checkbox" name="">Working Abroad<br>
+	 <input type="checkbox" name="">KU Commuter Work Experience<br>
+	 </form>
+         </td>
+	 </tr>
+	 </tr>
+	 </table>
+	 </div>
+  </div>
+  <div class = "three">
+  
+
+  </div>
+  </div>
+  <div class ="row">
+  	 <div class = "section 3">
+	<div class = "clear0"> In 2006 Kingston University introduced governing bodies to assist in students gain work experience</div>
+	<div class = "clear1"><b>CEB Insight Day at KU</b>.CEB is the best practice insight and technology company.
+	With more than 30 years of experience, they are the global market leader in talent management solutions,assessment and training</div>
+	<div class = "clear2"><a href = ""> View Latest Job News</a></div>
+	<div class = "clear0"> In 2006 Kingston University introduced governing bodies to assist in students gain work experience</div>
+	<div class = "clear4"> Colt Technology Services - Grad scheme Promotional Event - Register NOW
+	<button style = "position:relative; top:40px;"><a  href = "">REGISTER</a></button></div>
+	</div>
+    </div>
+  </body>
+</html>
+Contact GitHub API Training Shop Blog About
+© 2017 GitHub, Inc. Terms Privacy Security Status Help
